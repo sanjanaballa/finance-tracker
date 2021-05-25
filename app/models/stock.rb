b9eb571f21fdfc611b73ed3f789b216cd34e9f44
sanjanaballa <sanjana.balla@gmail.com>
2021-05-24 22:56:29 -0700

    add many-to-many association between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+	has_many :user_stocks
+  	has_many :users, through: :user_stocks
+
+  	validates :name, :ticker, presence: true
 	def self.new_lookup(ticker_symbol)
 		client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://sandbox.iexapis.com/v1')
@@ -8,4 +12,8 @@ class Stock < ApplicationRecord
 	      return nil
 	    end
   	end
+  	
+  def self.check_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
+  end
 end
