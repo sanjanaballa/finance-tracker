9ed7d826af3ee8abb7fc2995933a81c1f8389771
sanjanaballa <sanjana.balla@gmail.com>
2021-05-25 08:15:51 -0700

    add remove friend tracking functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
-  get 'users/my_portfolio'
   devise_for :users
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
+  resources :friendships, only: [:create, :destroy]
 end
